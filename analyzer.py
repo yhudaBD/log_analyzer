12d0e0e00be15ffc5a80f73de543804ff19135dd
Yhuda ben dayan <0527196317ybd@gmail.com>
2026-02-11 14:08:18 +0200

    implement traffic labeling

--- analyzer.py
+++ analyzer.py
@@ -10,5 +10,18 @@ def get_ip_in_log(log_data):
     return suspects_ip 
 
 log  = get_ip_in_log(read_log("network_traffic.log"))
-print(len(log))
+
+
+def tag_logs_by_size(log_data):
+   tagged_logs = []
+   for line in log_data:
+      if int(line[5]) > 5000:
+         line.insert(0 ,"LARGE")
+         tagged_logs.append(line)
+
+      else:
+         line.insert(0, "NORMAL" ) 
+         tagged_logs.append(line)
+   return tagged_logs  
+line = tag_logs_by_size(read_log("network_traffic.log"))
 
