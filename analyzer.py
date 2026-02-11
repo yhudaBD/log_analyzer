7aebbbe4a3c8466d45ca08709e8d590859e6fd69
Yhuda ben dayan <0527196317ybd@gmail.com>
2026-02-11 12:14:42 +0200

    adedd func check ip

--- analyzer.py
+++ analyzer.py
@@ -1,18 +1,14 @@
 from reader import read_log
 
 def get_ip_in_log(log_data):
-    counter = 0
     suspects_ip = []
-    #ip = log_data[1]
     for line in log_data:
       ip = line[1]
       if not ip.startswith("10.") and not ip.startswith("192.168."):
          suspects_ip.append(ip)
-         counter += 1
-    return suspects_ip ,counter
+         
+    return suspects_ip 
 
-log , count = get_ip_in_log(read_log("network_traffic.log"))
-print(f"Total entries: {count}")
-for ip in set(log):   
- occurrence = log.count(ip)
- print(f"IP Address: {ip} | Occurrences: {occurrence}")
+log  = get_ip_in_log(read_log("network_traffic.log"))
+print(len(log))
+
