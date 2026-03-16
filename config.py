27cc168afbf4831a11c4202eb6bb9ed740aa9c7b
Yhuda ben dayan <0527196317ybd@gmail.com>
2026-03-16 22:00:43 +0200

    Isolating the hour

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 from reader import read_log
+from datetime import datetime
+from main import log_data
 #1.3
 def filter_sensitive_ports(log_data):
      list_port_sens = []
@@ -7,4 +9,7 @@ def filter_sensitive_ports(log_data):
             list_port_sens.append(line)
      return list_port_sens
 liie = filter_sensitive_ports(read_log("network_traffic.log"))
-print(len(liie))       
+
+
+#3.1
+hours = list(map(lambda line: datetime.strptime(line[0], "%Y-%m-%d %H:%M:%S").hour, log_data))
