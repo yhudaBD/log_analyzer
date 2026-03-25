d9ed14521e602d26aee26fe8c10590aaefc935bf
Yhuda ben dayan <0527196317ybd@gmail.com>
2026-03-26 00:18:48 +0200

    Add port filtering using lambda and filter

--- config.py
+++ config.py
@@ -13,3 +13,8 @@ liie = filter_sensitive_ports(read_log("network_traffic.log"))
 
 #3.1
 hours = list(map(lambda line: datetime.strptime(line[0], "%Y-%m-%d %H:%M:%S").hour, log_data))
+
+#3.3
+Port_Filtering = list(
+    filter(lambda line:
+     line[3] == "22" or line[3] == "23" or line[3] == "3389" , log_data ))
